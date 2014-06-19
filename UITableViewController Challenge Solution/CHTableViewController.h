e8558d9e60967392a34e0006ac48d4463039e5b3
Arash Sadeghieh E <Arash@Arashs-MacBook-Pro.local>
2014-06-19 11:18:00 +1000

    Challenge Solved

---
+++ UITableViewController Challenge Solution/CHTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  CHTableViewController.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Arash Sadeghieh E on 19/06/2014.
+//  Copyright (c) 2014 Arash Sadeghieh Eshtehadi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CHTableViewController : UITableViewController
+
+@end
